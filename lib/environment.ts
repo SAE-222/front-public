30a7e462cc6dbcd1cc4ccda4f64deaec803930da
Antoine Mille <a.mille@etud.u-picardie.fr>
2023-11-29 18:40:40 +0100

    Fix environment

--- lib/environment.ts
+++ lib/environment.ts
@@ -4,6 +4,6 @@ const CLIENT_URL = prod
   ? process.env.NEXT_PUBLIC_API_URL
   : "http://localhost:8393/api";
 
-const BACK_URL = prod ? process.env.SERVER_SIDE_API_URL : "http://localhost:8393";
+const BACK_URL = prod ? process.env.SERVER_SIDE_API_URL : "http://localhost:8393/api";
 
 export { CLIENT_URL, BACK_URL }
